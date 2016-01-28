93fdd30944b3ff3a11427f991662289a51d75f34
Oliver <oliver@axismedtech.com>
2016-01-27 16:37:32 -0800

    more fun with tests, all passing so far

--- src/test/java/hibcDecoder/HIBCTest.java
+++ src/test/java/hibcDecoder/HIBCTest.java
@@ -42,6 +42,7 @@ public class HIBCTest extends GWTTestCase {
 
         scanner = new HIBC();
         test = scanner.decode("+$$09053C001  ");
+        assertNotNull(test);
         assertEquals(test.getType(), HIBC.Type.LINE_2);
         assertTrue(test.getPropertyValue().equals(lot));
         assertTrue(test.getLink().equals(' '));
@@ -53,17 +54,102 @@ public class HIBCTest extends GWTTestCase {
         // 1. test double line barcodes with leading and trailing *
         ////////////////////////////////////////////////////////////////////////////////
 
-        scanner = new HIBC();
-        HIBC.Decoded test1 = scanner.decode("*+Z999009993020351/05271C");
+        HIBC scanner1 = new HIBC();
+        HIBC.Decoded test1 = scanner1.decode("*+Z999009993020351/05271C");
         assertNotNull(test1);
         assertNull(test1.getError());
         assertEquals(test1.getType(), HIBC.Type.CONCATENATED);
         assertEquals(test1.getLabelerId(), labelerId);
         assertEquals(test1.getProduct(), product);
+        assertTrue(test1.getUom().equals(uom));
+        assertTrue(test1.getCheck().equals(check));
+        assertNotNull(test1.getDate());
 
 
+        scanner1 = new HIBC();
+        test1 = scanner1.decode("+Z999009993020351/05271C*");
+        assertNotNull(test1);
+        assertNull(test1.getError());
+        assertEquals(test1.getType(), HIBC.Type.CONCATENATED);
+        assertEquals(test1.getLabelerId(), labelerId);
+        assertEquals(test1.getProduct(), product);
+        assertTrue(test1.getUom().equals(uom));
+        assertTrue(test1.getCheck().equals(check));
+        assertNotNull(test1.getDate());
+
+        scanner1 = new HIBC();
+        test1 = scanner1.decode("*+Z999009993020351/05271C*");
+        assertNotNull(test1);
+        assertNull(test1.getError());
+        assertEquals(test1.getType(), HIBC.Type.CONCATENATED);
+        assertEquals(test1.getLabelerId(), labelerId);
+        assertEquals(test1.getProduct(), product);
+        assertTrue(test1.getUom().equals(uom));
+        assertTrue(test1.getCheck().equals(check));
+        assertNotNull(test1.getDate());
 
 
+        ////////////////////////////////////////////////////////////////////////////////
+        // 2. test for line 1 and line 2 linking and handle, check and link characters
+        ////////////////////////////////////////////////////////////////////////////////
+
+        HIBC scanner2 = new HIBC();
+        HIBC.Decoded test2 = scanner2.decode("+Z999009993020351/");
+        assertNotNull(test2);
+        assertNull(test2.getError());
+        assertEquals(test2.getType(), HIBC.Type.LINE_1);
+        assertEquals(test2.getLabelerId(), labelerId);
+        assertEquals(test2.getProduct(), product);
+        assertTrue(test2.getUom().equals(uom));
+        assertTrue(test2.getCheck().equals('/'));
+
+        scanner2 = new HIBC();
+        test2 = scanner2.decode("+$$09053C001L/");
+        assertNotNull(test2);
+        assertNull(test2.getError());
+        assertEquals(test2.getType(), HIBC.Type.LINE_2);
+        assertNull(test2.getLabelerId());
+        assertNull(test2.getProduct());
+        assertTrue(test2.getPropertyValue().equals(lot));
+        assertTrue(test2.getLink().equals(link));
+        assertTrue(test2.getCheck().equals('/'));
+        assertNotNull(test2.getDate());
+
+        scanner2 = new HIBC();
+        test2 = scanner2.decode("+$$09053C001//");
+        assertNotNull(test2);
+        assertNull(test2.getError());
+        assertEquals(test2.getType(), HIBC.Type.LINE_2);
+        assertTrue(test2.getPropertyValue().equals(lot));
+        assertTrue(test2.getLink().equals('/'));
+        assertTrue(test2.getCheck().equals('/'));
+        assertNotNull(test2.getDate());
+
+
+        ////////////////////////////////////////////////////////////////////////////////
+        // 3. more concatenated barcodes
+        ////////////////////////////////////////////////////////////////////////////////
+
+        HIBC scanner3 = new HIBC();
+        HIBC.Decoded test3 = scanner3.decode("+Z999009993020351/05271C");
+        assertNull(test3.getError());
+        assertEquals(HIBC.Type.CONCATENATED, test3.getType());
+        assertEquals(labelerId, test3.getLabelerId());
+        assertEquals(product, test3.getProduct());
+        assertTrue(test3.getUom().equals(uom));
+        assertNotNull(test3.getDate());
+        assertTrue(test3.getCheck().equals(check));
+
+        scanner3 = new HIBC();
+        test3 = scanner3.decode("+Z999009993020351/$3C001C");
+        assertNull(test3.getError());
+        assertEquals(HIBC.Type.CONCATENATED, test3.getType());
+        assertEquals(labelerId, test3.getLabelerId());
+        assertEquals(product, test3.getProduct());
+        assertTrue(test3.getUom().equals(uom));
+        assertNull(test3.getDate());
+        assertTrue( test3.getPropertyValue().equals(lot));
+        assertTrue(test3.getCheck().equals(check));
 
 
 
